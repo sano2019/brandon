63426d7f911731941bfdce7ce6108252e27c533a
Sander Nobel <sander.nobel@protonmail.com>
2021-08-29 22:49:51 +0200

    testing build hook

--- components/Footer.js
+++ components/Footer.js
@@ -4,24 +4,7 @@ import { footerStyles } from 'styles'
 const Footer = () => (
   <footer>
     <p>
-      Proudly published with &nbsp;
-      <a
-        href="https://prismic.io"
-        target="_blank"
-        rel="noopener noreferrer"
-      >Prismic</a>
-      <br />
-      <a
-        href="https://prismic.io"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          className="footer-logo"
-          src="/images/logo-prismic.svg"
-          alt="Gray Prismic logo"
-        />
-      </a>
+      Copyright Sander Nobel 2021
     </p>
     <style jsx global>{footerStyles}</style>
   </footer>
